f32a042efa42009bfbc4abc2f7ee2fb89517c52e
iliandy <iandyli@gmail.com>
2017-08-18 20:06:06 -0500

    completed listOverlap algo

---
+++ listOverlap.py
@@ -0,0 +1,16 @@
+def lstOverlap(lst1, lst2):
+    # lst = []
+    #
+    # for num in set(lst1):
+    #     if num in lst1 and num in lst2:
+    #         lst.append(num)
+    #
+    # return lst
+
+    return [num for num in set(lst1) if num in lst1 and num in lst2]
+
+
+lst1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+lst2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+print lstOverlap(lst1, lst2)
