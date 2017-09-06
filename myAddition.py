f4b873feb2dd26d9cf7663af9ba0f29afafc6d4f
iliandy <iandyli@gmail.com>
2017-09-05 23:16:00 -0500

    completed primeNums.js algo

--- myAddition.py
+++ myAddition.py
@@ -1,6 +1,9 @@
 # diy addition without + operator
 def myAddition(num1, num2):
-    return num1 - -num2
-
+    # return num1 - -num2
+    if (num2 == 0):
+        return num1
+    else:
+        return myAddition(num1 ^ num2, (num1 & num2) << 1)
 
 print(myAddition(3, -8))
--- primeNums.js
+++ primeNums.js
@@ -1,14 +1,32 @@
 // return all prime numbers between 0 and 100
-const primeNums = () => {
-  let resArr = [];
-
-  // using sieve of Eratosthenes algo
-  for (let i = 0; i <= 100; i++) {
-    if (i < 10 && (i == 2 || i == 3 || i == 3 || i == 5 || i == 7) || (i > 10 && !(i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0))) {
-      resArr.push(i);
+const primeNums = (maxNum) => {
+//   let primes = [];
+//
+//   // using sieve of Eratosthenes algo
+//   for (let i = 0; i <= 100; i++) {
+//     if (i < 10 && (i == 2 || i == 3 || i == 3 || i == 5 || i == 7) || (i > 10 && !(i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0))) {
+//       primes.push(i);
+//     }
+//   }
+//   return primes;
+// }
+//
+  let primes = [2], isPrime;
+  // test only odd numbers, starting with 3
+  for (let i = 3; i < maxNum; i+=2){
+    isPrime = true;
+    // test number i against primes found thus far
+    for (let j = 0; j < primes.length; j++){
+      if (i % primes[j] == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      primes.push(i);
     }
   }
-  return resArr;
+  return primes;
 }
 
-console.log(primeNums());
+console.log(primeNums(300));
