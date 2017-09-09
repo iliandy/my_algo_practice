3c451cee695d4f4c0e4de075c682c30efeb9d96b
iliandy <iandyli@gmail.com>
2017-09-09 17:13:07 -0500

    completed singleNumber.js algo via dict/hash/obj and math methods

--- singleNumber.js
+++ singleNumber.js
@@ -4,13 +4,32 @@
 // Note:
 // Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 const singleNumber = (arr) => {
-  let nums = {};
-  for (let num of arr) {
-    nums[num] = 1;
+  // // dictionary/hash/object method
+  // let nums = {};
+  // for (let num of arr) {
+  //   if (!(num in nums)) {
+  //     nums[num] = 1;
+  //   }
+  //   else {
+  //     nums[num] += 1;
+  //   }
+  // }
+  // return Object.keys(nums).find(key => nums[key] === 1);
+
+  // math method
+  // create set of unique values of arr
+  let arrSet = new Set(arr);
+
+  // callback function to sum array
+  const getSum = (total, num) => {
+    return total + num;
   }
-  console.log(nums);
-  return Object.keys(nums).find(key => nums[key] === 1);
+
+  let sumSet = [...arrSet].reduce(getSum, 0);
+  let sumArr = arr.reduce(getSum, 0);
+  // sum of double unique values - sum of arr = single unique element
+  return 2 * sumSet - sumArr
 }
 
-const arr = [1,1,2,2,3,4,4,6,6,0,0]
+const arr = [1, 1, 2, 2, 3, 4, 4, 6, 6, 0, 0]
 console.log(singleNumber(arr));
