903a1e8b487af16332926a41a0ec489020a1cb3b
iliandy <iandyli@gmail.com>
2017-09-19 23:32:17 -0500

    completed myAddition.py via built-in __add__ method

--- myAddition.py
+++ myAddition.py
@@ -1,9 +1,11 @@
 # diy addition without + operator
 def myAddition(num1, num2):
     # return num1 - -num2
-    if (num2 == 0):
-        return num1
-    else:
-        return myAddition(num1 ^ num2, (num1 & num2) << 1)
+	return (num1).__add__(num2)
+	
+    # if (num2 == 0):
+        # return num1
+    # else:
+        # return myAddition(num1 ^ num2, (num1 & num2) << 1)
 
 print(myAddition(3, -8))
