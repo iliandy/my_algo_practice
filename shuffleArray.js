9d70da632e0af6405927b06c60b6c5438f7811ad
iliandy <iandyli@gmail.com>
2018-07-08 23:53:16 -0500

    Added comments to shuffleArray.js

--- shuffleArray.js
+++ shuffleArray.js
@@ -1,19 +1,25 @@
+// Durstenfeld Shuffle, O(n) time complexity
+// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
 const shuffle0 = (arr) => {
   for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]]
+    const j = Math.floor(Math.random() * (i + 1));  // 0 ≤ j ≤ i
+    // const temp = arr[i]
+    // arr[i] = arr[j]
+    // arr[j] = temp
+    [arr[i], arr[j]] = [arr[j], arr[i]]   // es6 destructuring assignment, ie swap
   }
   return arr
 }
 
 const shuffle1 = (arr) => {
   for (let i = 0; i < arr.length - 1; i++) {
-    const j = Math.floor(Math.random() * (arr.length - i)) + i;
+    const j = Math.floor(Math.random() * (arr.length - i)) + i;   // i ≤ j < n
     [arr[i], arr[j]] = [arr[j], arr[i]]
   }
   return arr
 }
 
+// Should be avoided, does not randomize as well and slower
 const shuffle2 = (arr) => {
   return arr.sort(() => {
     return 0.5 - Math.random()
